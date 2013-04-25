c3db45ba032a429c004e22bc8ebf91f9692f7788
Lucio Baglione <coire1@gmail.com>
2013-04-25 15:16:15 +0200

    Make query on Object "Partita" and display results

--- js/app.js
+++ js/app.js
@@ -1,75 +1,118 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
-	function onDeviceReady() {
-		
-		
-		Parse.initialize( 'LkaGTOk7RGUaPXM0r9HQImwPAnmqUuhjF1QttcNE' , 'uxXxR7sEt2unuSABRyjF8tnd52bNymwlDuchsIhh' );
+function onDeviceReady() {
+	Parse.initialize( 'LkaGTOk7RGUaPXM0r9HQImwPAnmqUuhjF1QttcNE' , 'uxXxR7sEt2unuSABRyjF8tnd52bNymwlDuchsIhh' );
+	document.addEventListener("pause", onPause, false);
+	document.addEventListener("resume", onResume, false);
 
-		document.addEventListener("pause", onPause, false);
-		document.addEventListener("resume", onResume, false);
+  //Partita Object
+  var OPartita = Parse.Object.extend("Partita", {
+    initialize: function() {
 
-  // Lista partite View
- var VListaPartite = Parse.View.extend({
- 	
- 	el: $("#container"),
- 	
- 	initialize: function() {
-      this.render();
+    }
+  });
+
+  //Partite Collection
+  var CPartite = Parse.Collection.extend({
+    model: OPartita,
+    filter: function(){
+    }
+  });
+
+  // Partita View
+  var VPartitaPlayer = Parse.View.extend({
+    el: $('#container'),
+
+    initialize: function() {
+      this.model.bind('change', this.render);
     },
- 	render: function() {
-		var sourceLogout = $('#logout_template').html();
-        var templateLogout = Handlebars.compile(sourceLogout);
-        var html = templateLogout();
-        this.$el.html(html);
-        return this;
-   },
+
+    render: function() {
+      var sourcePartitaPlayer = $('#partita_player_template').html();
+      var templatePartitaPlayer = Handlebars.compile(sourcePartitaPlayer);
+      var html = templatePartitaPlayer();
+      this.$el.html(html);
+      return this;
+    }
+  });
+  // Lista partite View
+  var VListaPartite = Parse.View.extend({	
+ 	  el: $("#container"),
+
+ 	  initialize: function() {
+      var self = this;
+      // Initialize Parse.Query on Object OPartita
+      var query = new Parse.Query(OPartita);
+      query.find({
+        success: function(results) {
+          // Initialize empty array of results linked to current View
+          self.collection = [];
+          // On success Callback iterates through results and transforms every object to JSON (for Handlebars)
+          for(var i = 0; i < results.length; i++) {
+            self.collection.push(results[i].toJSON());
+          }
+          // Renders current View on Query success
+          self.render();
+        },
+        error: function(error) {
+          // error is an instance of Parse.Error.
+        }
+      });
+    },
+
+ 	  render: function() {
+		  var sourceLogout = $('#logout_template').html();
+      var sourceListaPartite = $('#lista_partite_template').html();
+      var templateLogout = Handlebars.compile(sourceLogout);
+      var templateListaPartite = Handlebars.compile(sourceListaPartite);
+      // Renders #logout_template and #lista_partite_template
+      // #lista_partite_template displays a list of match iterating through "partite"
+      var html = templateLogout()+templateListaPartite({partite: this.collection});
+      this.$el.html(html);
+      return this;
+    },
    
-   // Logs out the user and shows the login view
-   logout: function(e) {
+    // Logs out the user and shows the first App View 
+    logout: function(e) {
       var self = this;
       Parse.User.logOut();
       new AppView();
       self.undelegateEvents();
       delete self;
-	  this.$(".Slogout button").attr("disabled", "disabled");
+      this.$(".Slogout button").attr("disabled", "disabled");
       return false;
     },
-    
     events: {       
-    			"submit form.Slogout": "logout"
+      "submit form.Slogout": "logout"
     }
- });
+  });
  	
  	
   // The login view for the app
   var VLogSign = Parse.View.extend({
-
     el: $("#container"),
-
     initialize: function() {
       this.render();
     },
-    
     events: {       
-    			"submit form.Slogin": "login",
-      			"submit form.Ssignup": "signup" 
+    	"submit form.Slogin": "login",
+      "submit form.Ssignup": "signup"
     },
 
     render: function() {
-		var sourceLogin = $('#login_template').html();
-		var sourceSignup = $('#signup_template').html();
-        var templateLogin = Handlebars.compile(sourceLogin);
-        var templateSignup = Handlebars.compile(sourceSignup);
-        var html = templateLogin()+templateSignup();
-        this.$el.html(html);
-        return this;
+		  var sourceLogin = $('#login_template').html();
+		  var sourceSignup = $('#signup_template').html();
+      var templateLogin = Handlebars.compile(sourceLogin);
+      var templateSignup = Handlebars.compile(sourceSignup);
+      var html = templateLogin()+templateSignup();
+      this.$el.html(html);
+      return this;
     },
     
     signup: function(){
       var self = this;
       var username = this.$("#signup-username").val();
       var password = this.$("#signup-password").val();
-      
       Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
         success: function(user) {
           new VListaPartite();
@@ -82,32 +125,26 @@ document.addEventListener("deviceready", onDeviceReady, false);
           this.$(".Ssignup button").removeAttr("disabled");
         }
       });
-
       this.$(".Ssignup button").attr("disabled", "disabled");
-
       return false;	
     },
     
     login: function(){
       var self = this;
       var username = this.$("#login-username").val();
-      var password = this.$("#login-password").val();
-      
+      var password = this.$("#login-password").val();  
       Parse.User.logIn(username, password, {
         success: function(user) {
           new VListaPartite();
           self.undelegateEvents();
           delete self;
         },
-
         error: function(user, error) {
           self.$(".Slogin .error").html("Invalid username or password. Please try again.").show();
           this.$(".Slogin button").removeAttr("disabled");
         }
       });
-
       this.$(".login-form button").attr("disabled", "disabled");
-
       return false;
     }
   });
@@ -151,16 +188,13 @@ document.addEventListener("deviceready", onDeviceReady, false);
 */    
 	}
 
-			//What to do when paused
-		function onPause(){
-			
-			alert("paused!");
-		}
+//What to do when paused
+function onPause() {
+	alert("paused!");
+}
 		
-		//What to do when resumed
-		function onResume()
-		{
-			
-			alert("resume");
-		}	
+//What to do when resumed
+function onResume()	{
+	alert("resume");
+}	
 
