1dba91795751a1047244c09e7ecf0d5499b80b34
Lucio Baglione <coire1@gmail.com>
2013-05-27 20:52:21 +0200

    Added Model Pms

---
+++ js/models/Pms.js
@@ -0,0 +1,29 @@
+/*jslint undef: true*/
+/*global define*/
+
+define([
+  "jquery",
+  "Parse"
+],
+  function ($, Parse) {
+    var Pms = Parse.Object.extend("Pms", {
+      roles: {'MASTER': 0, 'LISTED': 1, 'INVITED': 2, 'INGAME': 3},
+      saveMaster: function (matchId) {
+        var self = this;
+        this.save({
+          userId: Parse.User.current().id,
+          matchId: matchId,
+          state: self.states.MASTER
+        }, {
+          success: function (result) {
+            self.trigger('NUOVA_PARTITA_VM_PMSMASTERCREATED');
+          },
+          error: function (e) {
+
+          }
+        });
+      }
+    });
+    return Invite;
+
+  });
